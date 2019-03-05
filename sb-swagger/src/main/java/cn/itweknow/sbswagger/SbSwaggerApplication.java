0e3f1369e50b82c85dafbd7e8ed1c75e05f8bcb1
甘朝阳 <ganchaoyang@tuhu.cn>
2019-03-05 19:26:43 +0800

    springboot项目集成swagger2架子搭建

---
+++ sb-swagger/src/main/java/cn/itweknow/sbswagger/SbSwaggerApplication.java
@@ -0,0 +1,13 @@
+package cn.itweknow.sbswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbSwaggerApplication.class, args);
+    }
+
+}
---
+++ sb-swagger/src/test/java/cn/itweknow/sbswagger/SbSwaggerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.itweknow.sbswagger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbSwaggerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
