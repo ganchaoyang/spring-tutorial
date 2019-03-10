1df0036fc20da8aaee80d131afc6e31b59f5e5c3
甘朝阳 <ganchaoyang@tuhu.cn>
2019-03-10 22:03:08 +0800

    删除无用信息

--- sb-swagger/src/main/java/cn/itweknow/sbswagger/config/SwaggerConfig.java
+++ sb-swagger/src/main/java/cn/itweknow/sbswagger/config/SwaggerConfig.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2019 tuhu.cn All right reserved. This software is the
- * confidential and proprietary information of tuhu.cn ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Tuhu.cn
- */
 package cn.itweknow.sbswagger.config;
 
 import com.google.common.base.Predicates;
@@ -20,8 +13,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
 import java.util.Collections;
 
 import static com.google.common.collect.Lists.newArrayList;
--- sb-swagger/src/main/java/cn/itweknow/sbswagger/controller/UserController.java
+++ sb-swagger/src/main/java/cn/itweknow/sbswagger/controller/UserController.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2019 tuhu.cn All right reserved. This software is the
- * confidential and proprietary information of tuhu.cn ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Tuhu.cn
- */
 package cn.itweknow.sbswagger.controller;
 
 import cn.itweknow.sbswagger.model.User;
