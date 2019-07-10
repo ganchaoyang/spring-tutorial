23428d630407a920e3abb02c169a386206d83916
ganchaoyang <gancy.programmer@gmail.com>
2019-07-10 22:04:53 +0800

    Spring Boot配置数据源

---
+++ sb-datasource/src/main/java/cn/itweknow/sbdatasource/SbDatasourceApplication.java
@@ -0,0 +1,13 @@
+package cn.itweknow.sbdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDatasourceApplication.class, args);
+	}
+
+}
